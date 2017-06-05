d11adf68ab091b84763f60df8e920530ace6ad46
osm0sis <osm0sis@outlook.com>
2017-06-05 11:54:50 +0000

    Scripts : Anykernel 2 Implementation

---
+++ out/anykernel.sh
@@ -0,0 +1,47 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=Trigon by @TheDarkStrix
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=kenzo
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel permissions
+# set permissions for included ramdisk files
+chmod -R 755 $ramdisk
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# insert qcom.power.rc
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
+
+
